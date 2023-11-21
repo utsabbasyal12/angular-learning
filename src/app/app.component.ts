0145bce684a239886256ceaeef34eacbd3c9890b
utsav.basyal <45966409+utsabbasyal12@users.noreply.github.com>
2023-11-21 17:28:41 +0545

    OnInit method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { TimeoutError } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-learning';
+export class AppComponent  implements OnInit{
+  intervalSub: any;
+  ngOnInit(): void {
+      this.intervalSub = setInterval(() => {
+        console.log('Hello from ngOnInit');
+      }, 1000);
+  }
 }
