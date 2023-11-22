a8f4861505958c8fd19b9d3b90c1fe6260aea846
utsav.basyal <45966409+utsabbasyal12@users.noreply.github.com>
2023-11-22 11:14:08 +0545

    Component Communication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
+    HelloWorldComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
