f4abb11051e180706e7fea2928deebf9310eff58
utsav.basyal <45966409+utsabbasyal12@users.noreply.github.com>
2024-01-14 17:07:09 +0545

    added routing in login and dashboard components in sidenavbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './hello-world.component';
-import { ParentComponent } from './parent/parent.component';
-import { ChildComponent } from './child/child.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidbarComponent } from './sidbar/sidbar.component';
+import { UserRegisterComponent } from './user-register/user-register.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent,
-    ParentComponent,
-    ChildComponent
+    NavbarComponent,
+    SidbarComponent,
+    UserRegisterComponent,
+    LoginComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
