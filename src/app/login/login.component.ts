a6703b1a079f91d686bb87255aa8ebe564a7cdb9
utsav.basyal <45966409+utsabbasyal12@users.noreply.github.com>
2024-01-16 11:43:55 +0545

    added login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faFacebook, faGoogle, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  myLinkedInIcon = faLinkedinIn;
+  myFacebookIcon = faFacebook;
+  myGoogleIcon = faGoogle;
 }
